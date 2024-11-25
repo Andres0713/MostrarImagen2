7c5ec078c40b64718c9debfd44e036b78bc79bcd
Andres0713 <opertop.es.74863@gmail.com>
2024-11-25 14:32:16 -0600

    segundo

--- src/main/java/com/ejemplo/SftpConfig.java
+++ src/main/java/com/ejemplo/SftpConfig.java
@@ -1,7 +1,7 @@
 package com.ejemplo;
 
 public class SftpConfig {
-    public static final String SFTP_HOST = "192.168.100.212";
+    public static final String SFTP_HOST = "10.224.2.47";
     public static final int SFTP_PORT = 22;
     public static final String SFTP_USER = "administrador";
     public static final String SFTP_PASSWORD = "Abcd2024";
